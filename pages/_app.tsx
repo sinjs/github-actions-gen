085c2387354aa643bad52eed59ce4367fef64ec9
sinmineryt <50749166+sinmineryt@users.noreply.github.com>
2021-07-21 18:29:17 +0200

    Modify _app to show the TopBar and apply styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import useStyles from "../components/useStyles";
+import TopBar from "../components/TopBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <TopBar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
-export default MyApp
+export default MyApp;
