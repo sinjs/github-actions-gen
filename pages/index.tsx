c2e80f8aa49320ecf38d1f08ee8c3cc6b6dc8b48
sinmineryt <50749166+sinmineryt@users.noreply.github.com>
2021-07-21 18:58:18 +0200

    Add Workflow name field

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,18 @@
+import Container from "@material-ui/core/Container";
+import TextField from "@material-ui/core/TextField";
+import useStyles from "../components/useStyles";
+
 export default function Home() {
-  return <>Hello, world!</>;
+  const classes = useStyles();
+
+  return (
+    <Container style={{ marginTop: "24px" }} maxWidth="xl">
+      <TextField
+        required
+        label="Workflow name"
+        defaultValue="CI"
+        variant="filled"
+      />
+    </Container>
+  );
 }
